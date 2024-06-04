7dadf83475af393f045f3c656b4a5f544a56e226
Bruno Lima <45238081+brect@users.noreply.github.com>
2024-06-04 00:02:39 -0300

    Adiciona providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,35 @@
+import { ConsultarPedidosComponent } from './componentes/pedidos/consultar-pedidos/consultar-pedidos.component';
+import { ConsultarClientesComponent } from './componentes/clientes/consultar-clientes/consultar-clientes.component';
+import { LoginComponent } from './home/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './home/login/login.component';
-import { NovoUsuarioComponent } from './home/novo-usuario/novo-usuario.component';
-import { MenuComponent } from './componentes/menu/menu.component';
-import { MensagemComponent } from './componentes/mensagem/mensagem.component';
-import { CadastrarClientesComponent } from './componentes/clientes/cadastrar-clientes/cadastrar-clientes.component';
-import { ConsultarClientesComponent } from './componentes/clientes/consultar-clientes/consultar-clientes.component';
-import { EditarClientesComponent } from './componentes/clientes/editar-clientes/editar-clientes.component';
-import { ExcluirClientesComponent } from './componentes/clientes/excluir-clientes/excluir-clientes.component';
-import { CadastrarPedidosComponent } from './componentes/pedidos/cadastrar-pedidos/cadastrar-pedidos.component';
-import { ConsultarPedidosComponent } from './componentes/pedidos/consultar-pedidos/consultar-pedidos.component';
-import { EmitePedidoComponent } from './componentes/pedidos/emite-pedido/emite-pedido.component';
-import { ValidandoCepDirective } from './directives/validando-cep.directive';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { CadastraClientesComponent } from './componentes/clientes/cadastra-clientes/cadastra-clientes.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NovoUsuarioComponent } from './home/novo-usuario/novo-usuario.component';
+import { MensagemComponent } from './componentes/mensagem/mensagem.component';
+import { EmitePedidoComponent } from './componentes/pedidos/emite-pedido/emite-pedido.component';
+import { CadastrarPedidosComponent } from './componentes/pedidos/cadastrar-pedidos/cadastrar-pedidos.component';
+import { ExcluirPedidosComponent } from './componentes/pedidos/excluir-pedidos/excluir-pedidos.component';
+import { EditarPedidosComponent } from './componentes/pedidos/editar-pedidos/editar-pedidos.component';
+import { ValidandoCepDirective } from './directives/validando-cep.directive';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { AsyncPipe } from '@angular/common';
+import {importProvidersFrom} from '@angular/core';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CabecalhoComponent,
-    RodapeComponent,
-    HomeComponent,
-    LoginComponent,
-    NovoUsuarioComponent,
-    MenuComponent,
-    MensagemComponent,
-    CadastrarClientesComponent,
-    ConsultarClientesComponent,
-    EditarClientesComponent,
-    ExcluirClientesComponent,
-    CadastrarPedidosComponent,
-    ConsultarPedidosComponent,
-    EmitePedidoComponent,
-    ValidandoCepDirective,
-  ],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, HomeComponent, LoginComponent, MensagemComponent, NovoUsuarioComponent, MenuComponent, CadastraClientesComponent, ConsultarClientesComponent, CadastrarPedidosComponent, ConsultarPedidosComponent, ExcluirPedidosComponent, EditarPedidosComponent ,EmitePedidoComponent, ValidandoCepDirective],
   imports: [
     AppRoutingModule,
     HttpClientModule,
@@ -54,7 +41,11 @@ import { AsyncPipe } from '@angular/common';
     MatAutocompleteModule,
     AsyncPipe,
   ],
-  providers: [],
+  providers: [
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(MatNativeDateModule)
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
