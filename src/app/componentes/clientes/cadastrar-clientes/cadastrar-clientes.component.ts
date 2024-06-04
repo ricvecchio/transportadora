ec6047df641cc6099b5252530afb2b0939613ccb
Bruno Lima <45238081+brect@users.noreply.github.com>
2024-06-04 00:03:25 -0300

    Ajusta componente de formulario de emitir

--- src/app/componentes/clientes/cadastrar-clientes/cadastrar-clientes.component.ts
+++ src/app/componentes/clientes/cadastrar-clientes/cadastrar-clientes.component.ts
@@ -1,44 +1,138 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { Component, Input, OnInit } from '@angular/core';
-import { Cliente } from '../cliente';
-import { ClienteService } from '../cliente.service';
+import { ConsultaCepService } from '../../../service/consulta-cep.service';
+import { Observable, map, of, startWith } from 'rxjs';
+
+export interface User {
+  name: string;
+}
+/**
+ * @title Display value autocomplete
+ */
 
 @Component({
-  selector: 'app-cadastrar-clientes',
-  templateUrl: './cadastrar-clientes.component.html',
-  styleUrl: './cadastrar-clientes.component.css',
+  selector: 'app-cadastra-clientes',
+  templateUrl: './cadastra-clientes.component.html',
+  styleUrl: './cadastra-clientes.component.css',
 })
-export class CadastrarClientesComponent implements OnInit {
-  @Input() dadosPedido: Cliente = {
-    fantasia: '',
-    razaosocial: '',
+export class CadastraClientesComponent implements OnInit {
+  
+  dadosCliente = {
+    idCliente: '1',
+    nome: 'Ricardo Del Vecchio (TESTANDO)',
+    cpf: '',
     telefone: '',
+    celular: '',
+    email: '',
+    cep: '',
+    logradouro: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+    fantasia: '',
+    razaoSocial: '',
+    cnpj: '',
+    tipoPgto: '',
+    sfobras: '',
+    cno: '',
+    mangueira: '',
+    ie: '',
+    volume: '',
+    ajudanteHora: '',
+    observacao: '',
     modelo: 'modelo1',
   };
 
-  larguraCliente(): string {
-    if (this.dadosPedido.razaosocial.length >= 256) {
-      return 'cliente-g';
-    }
-    return 'cliente-p';
-  }
+  // formulario!: FormGroup;
+
+  myControl = new FormControl<string | User>('');
+
+  options: User[] = [
+    { name: 'Jerinelson' }, 
+    { name: 'Bruno' }, 
+    { name: 'Brunelson' }, 
+    { name: 'Brunolindo' },
+    { name: 'Ricardo 01' }, 
+    { name: 'Ricardo 02' },  
+    { name: 'Ricardo 03' }, 
+    { name: 'Ricardo Del' }];
+
+    filteredOptions: Observable<User[]> = of([]);
 
   constructor(
-    private service: ClienteService,
     private router: Router,
+    private consultaCepService: ConsultaCepService,
+    // private formBuilder: FormBuilder,
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
 
-  cadastrarCliente() {
-    this.service.criar(this.dadosPedido).subscribe(() => {
-      alert("Cliente cadastrado com sucesso!")
-      this.router.navigate(['/consultar-clientes']);
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.name.toLowerCase().includes(filterValue),
+    );
+  }
+
+  consultaCEP(ev: any, form: NgForm) {
+    const cep = ev.target.value;
+    if (cep != '') {
+      this.consultaCepService.getConsultaCep(cep).subscribe((resultado) => {
+        console.log(resultado);
+        this.populandoEndereco(resultado, form);
+      });
+    }
+  }
+
+  populandoEndereco(dados: any, form: NgForm) {
+    form.form.patchValue({
+      logradouro: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf,
     });
   }
 
-  cancelar() {
-    this.router.navigate(['/menu'])
+  // cadastrar(form: NgForm){
+  //   // console.log('Cliente cadastrado com sucesso!');
+  //   console.log(form.controls);
+  // }
+
+  cadastrar(form: any) {
+    console.log(form);
+    if (form.valid) {
+      this.router.navigate(['sucesso']);
+      console.log('Formulário enviado');
+      alert('Formulário enviado');
+    } else {
+      console.log('Formulário inválido');
+      alert('Formulário inválido!');
+    }
   }
 
+  cancelar() {
+    alert('Ação cancelada!');
+  }
 }
