1212aa5fd0e9f7617082f744a47c6137c1bb44ed
ricardo <ric.vecchio@gmail.com>
2024-07-12 11:42:11 -0300

    Refatorando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
-import { RodapeComponent } from './rodape/rodape.component';
 import { HomeComponent } from './home/home.component';
-import { NovoUsuarioComponent } from './home/novo-usuario/novo-usuario.component';
-import { MensagemComponent } from './home/mensagem/mensagem.component';
 import { LoginComponent } from './home/login/login.component';
+import { MensagemComponent } from './home/mensagem/mensagem.component';
+import { NovoUsuarioComponent } from './home/novo-usuario/novo-usuario.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    MatToolbarModule,
-    HttpClientModule,
+  declarations: [
+    AppComponent,
     CabecalhoComponent,
     RodapeComponent,
     HomeComponent,
     LoginComponent,
     NovoUsuarioComponent,
+    MenuComponent,
     MensagemComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    MatToolbarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
