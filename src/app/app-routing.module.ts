17d517ca4f2c485783d2d4407e4897300afc980f
ricardo <ric.vecchio@gmail.com>
2024-07-16 11:28:44 -0300

    Excluindo comentarios e identando app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MenuComponent } from './menu/menu.component';
+import { ClientesListaComponent } from './clientes/componentes/clientes-lista/clientes-lista.component';
+import { ClienteFormComponent } from './clientes/containers/cliente-form/cliente-form.component';
+import { ClienteResolver } from './clientes/guarda-rotas/cliente.resolver';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './home/login/login.component';
 import { NovoUsuarioComponent } from './home/novo-usuario/novo-usuario.component';
-import { ClienteFormComponent } from './clientes/containers/cliente-form/cliente-form.component';
-import { ClientesListaComponent } from './clientes/componentes/clientes-lista/clientes-lista.component';
-import { ClienteResolver } from './clientes/guarda-rotas/cliente.resolver';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   {
@@ -31,26 +31,24 @@ const routes: Routes = [
   },
   {
     path: 'menu',
-    component: MenuComponent
+    component: MenuComponent,
   },
-  { path: 'cadastrar-cliente', component: ClienteFormComponent, resolve: { cliente: ClienteResolver} },
-  { path: 'consultar-clientes', component: ClientesListaComponent, resolve: { cliente: ClienteResolver} },
-  { path: 'editar-cliente/:id', component: ClienteFormComponent, resolve: { cliente: ClienteResolver} },
+  {
+    path: 'cadastrar-cliente',
+    component: ClienteFormComponent,
+    resolve: { cliente: ClienteResolver },
+  },
+  {
+    path: 'consultar-clientes',
+    component: ClientesListaComponent,
+    resolve: { cliente: ClienteResolver },
+  },
+  {
+    path: 'editar-cliente/:id',
+    component: ClienteFormComponent,
+    resolve: { cliente: ClienteResolver },
+  },
 
-  // {
-  //   path: 'clientes',
-  //   loadChildren: () => import('./clientes/clientes.module').then(m => m.ClientesModule)
-  // },
-
-  // { path: 'deletar-cliente/:idCliente', component: ClientesListaComponent, resolve: { cliente: ClienteResolver} },
-  // {
-  //   path: 'clientes/editar-clientes/:id',
-  //   component: EditarClientesComponent
-  // },
-  // {
-  //   path: 'clientes/excluir-clientes/:id',
-  //   component: ExcluirClientesComponent
-  // },
   // {
   //   path: 'cadastrar-pedidos',
   //   component: CadastrarPedidosComponent
